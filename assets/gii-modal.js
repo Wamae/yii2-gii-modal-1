f3d23352f0ea8f78facb16b8a4a781495f9ed0a2
Andrey Borodulin <borodulin@gmail.com>
2015-10-23 16:02:49 +0600

    show errors

--- assets/gii-modal.js
+++ assets/gii-modal.js
@@ -1,44 +1,62 @@
 $(function(){
     $(document).on('click', '.show-modal',  function(){
         var self = $(this),
-            target = $(self.attr('data-target')),
+        	target = $(self.attr('data-target')),
             ajax_url = self.attr('data-url') || self.attr('href'),
             header = self.attr('data-header')||'';
-        var h4 = target.find('.modal-header').find('h4');
-        if (h4.length === 0) {
-            $('<h4>'+header+'</h4>').appendTo(target.find('.modal-header'));
-        } else {
-            h4.text(header);
-        }
+		var h4 = target.find('.modal-header').find('h4');
+		if (h4.length === 0) {
+		    $('<h4>'+header+'</h4>').appendTo(target.find('.modal-header'));
+		} else {
+			h4.text(header);
+		}
         if (ajax_url) {
-            target.modal('show').find('.modal-body').empty().load(ajax_url);
-            return false;
+        	var body = target.modal('show').find('.modal-body').empty();
+        	$.ajax({
+    			url: ajax_url,
+    			success: function (response) {
+    				body.html(response);
+    			},
+    			error: function (jqXHR) {
+    				body.html('<div class="error-summary">'+jqXHR.responseText+'</div>');
+    			}
+    		});
+        	return false;
         }
     });
-    $('body').on('beforeSubmit', 'form[data-ajax]', function () {
-        var formData = new FormData(this);
-        var form = $(this);
-        // submit form
-        $.ajax({
-            url: form.attr('action'),
-            type: 'post',
-            enctype: 'multipart/form-data',
-            processData: false,  // tell jQuery not to process the data
-            contentType: false,   // tell jQuery not to set contentType
-            data: formData,
-            success: function (response) {
-                form.closest('.modal-body').html(response);
-            },
+	$('body').on('beforeSubmit', 'form[data-ajax]', function () {
+		var formData = new FormData(this);
+		var form = $(this);
+		var body = form.closest('.modal-body').empty();
+		// submit form
+		$.ajax({
+			url: form.attr('action'),
+			type: 'post',
+			enctype: 'multipart/form-data',
+			processData: false,  // tell jQuery not to process the data
+			contentType: false,   // tell jQuery not to set contentType
+			data: formData,
+			success: function (response) {
+				body.html(response);
+			},
 			error: function (jqXHR, textStatus, errorThrown) {
-				form.closest('.modal-body').html('<div class="error-summary">'+jqXHR.responseText+'</div>');
+				body.html('<div class="error-summary">'+jqXHR.responseText+'</div>');
 			}
-        });
-        return false;
-    });
-    $('body').on('click', 'a[data-ajax]', function () {
-        var self = $(this),
-            modal_body = self.closest('.modal-body');
-        modal_body.empty().load(self.attr('href'));
-        return false;
-    });
+		});
+		return false;
+	});
+	$('body').on('click', 'a[data-ajax]', function () {
+		var self = $(this),
+			modal_body = self.closest('.modal-body').empty();
+		$.ajax({
+			url: self.attr('href'),
+			success: function (response) {
+				modal_body.html(response);
+			},
+			error: function (jqXHR) {
+				modal_body.html('<div class="error-summary">'+jqXHR.responseText+'</div>');
+			}
+		});
+		return false;
+	});
 });
